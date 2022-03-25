cb9bfd6ba4605d65977c878e251c05ee6f3527eb
AmandaRichards <Richards.amanda92@gmail.com>
2022-03-25 15:50:39 +0000

    recipe box images and hover fixed

--- Components/RecipeBoxx/index.js
+++ Components/RecipeBoxx/index.js
@@ -2,22 +2,29 @@ import React from 'react';
 import Card from 'react-bootstrap/Card';
 import {Row} from 'react-bootstrap'
 import css from './RecipeBoxx.module.css'
+import Link from 'next/link'
 
-const RecipeBoxx = ({image, title, text}) => {
+const RecipeBoxx = ({image, title, text, link}) => {
     return (
         <Row >
-        <Card  style={{ width: '18rem' }} className={css.card} >
+        <Card  style={{ width: '18rem' }} className={`${css.card} ${css.tooltip}`}  >
+        <Link href={link}>
+        <span className={css.tooltiptext}>See recipe</span>
+        </Link>
+      <Row className={css.imagediv}> 
   <Card.Img variant="top" src={image} className={css.image}/>
+  </Row>  
+  <Row>
+  <Card.Body className={css.textRow} >
   
-  <Card.Body className={css.tooltip} >
-   <button  >
-   <span className={css.tooltiptext}>See recipe</span></button> 
+  
     <Card.Title>{title}</Card.Title>
     <Card.Text>
      {text}
     </Card.Text>
     
   </Card.Body>
+  </Row>
 </Card>
 </Row>
     )
